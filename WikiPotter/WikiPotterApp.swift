4d298f60b69381c06dbdd3d33c32009ade29d4c2
Rizal <rizalisme007@gmail.com>
2025-11-22 23:49:46 +0800

    Initial Commit

---
+++ WikiPotter/WikiPotterApp.swift
@@ -0,0 +1,32 @@
+//
+//  WikiPotterApp.swift
+//  WikiPotter
+//
+//  Created by Rizal Khanafi on 22/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WikiPotterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
